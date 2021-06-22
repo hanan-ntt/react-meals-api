93c1472ae45d1f4a2b0e8a8491f8fe9d770e19f2
Hanan Ahmed <hanan.ahmed@nttdata.com>
2021-06-22 17:29:41 +0100

    Successfully connected API post method to DB

--- controllers/mealsController.js
+++ controllers/mealsController.js
@@ -10,8 +10,9 @@ exports.getAllMeals = (req, res) => {
 };
 
 exports.createNewMeal = async (req, res) => {
-  try{
-    const newMeal = await Meals.create(req.body)
+  console.log(req);
+  try {
+    const newMeal = await Meals.create(req.body);
     res.status(200).json({
       status: 'success',
       data: {
@@ -20,8 +21,11 @@ exports.createNewMeal = async (req, res) => {
     });
   } catch (e) {
     res.status(400).json({
-      
-    })
+      status: 'fail',
+      data: {
+        message: e,
+      },
+    });
   }
 };
 
